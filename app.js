845da01beb441f980a82c84ba556ad09adf9a7b8
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-13 10:55:18 +0700

    J

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
+const dashboardRouter = require("./routes/dashboard");
+const publicRouter = require("./routes/public");
+const usersRouter = require("./routes/users");
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 //var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session')
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
 
 var app = express();
-
+var oktaClient = new okta.Client({
+  orgUrl: 'https://dev-427620.okta.com',
+  token: '00k_EPxgQo5OG0B8pHH8wNCrXfxYudwQbAkhOnbJ-m'
+});
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-427620.okta.com/oauth2/default",
+  client_id: '0oal0rgyao9PsaEKG356',
+  client_secret: 'kW2nHpxEmLhclB3AeylSVOT8ZFpEmVv95klETddh',
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -20,13 +45,39 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'kW2nHpxEmLhclB3AeylSVOT8ZFpEmVv95klETddh',
+  secret: 'dangtuanhuyhachi',
   resave: true,
   saveUninitialized: false
 }))
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(oidc.router);
 
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
+app.use('/dashboard', loginRequired, dashboardRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/', publicRouter);
+app.use('/dashboard', dashboardRouter);
+app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
